caac4ff552bdc553222ea844c9410021bff15c0e
Amayas Said Amer <saidameramayas@gmail.com>
2019-02-15 16:21:07 -0500

    Improve

--- Linked_list.py
+++ Linked_list.py
@@ -4,20 +4,16 @@ class Node:
 		self.nxt = nxt
 	def __repr__(self):
 		return str(self.data)
+	def __eq__(self,other):
+		if other == None:
+			return False
+		return self.data == other.data
 
 class Linked_list:
-	def __init__(self,start=None):
-		self.start = start
+	def __init__(self):
+		self.start = None
 		self.end = self.start
-	def append(self,node):
-		if self.start == None and self.end == None:
-			self.start = Node(node.data)
-			self.end = self.start
-			return
-		while self.end.nxt != None:
-			self.end = self.end.nxt
-		self.end.nxt = Node(node.data)
-		self.end = self.end.nxt
+		self.length = 0
 	def __repr__(self):
 		s = "LL(" + str(self.start)
 		current = self.start
@@ -34,30 +30,36 @@ class Linked_list:
 			current_this = current_this.nxt
 			current_other = current_other.nxt
 		return True
+	def append(self,obj):
+		if self.start == None and self.end == None:
+			self.start = Node(obj)
+			self.end = self.start
+			self.length += 1
+			return
+		while self.end.nxt != None:
+			self.end = self.end.nxt
+		self.length += 1
+		self.end.nxt = Node(obj)
+		self.end = self.end.nxt
 
 
 if __name__ == "__main__":
-	n1 = Node(1)
-	n2 = Node(2)
-	n3 = Node(3)
-	n4 = Node(4)
-	n5 = Node(5)
 
 	ll = Linked_list()
-	ll.append(n1)
-	ll.append(n2)
-	ll.append(n3)
-	ll.append(n4)
-	ll.append(n5)
+	ll.append(1)
+	ll.append(2)
+	ll.append(3)
+	ll.append(4)
+	ll.append(5)
 
 	print(ll)
 	print()
 
 	ll2 = Linked_list()
-	ll2.append(n1)
-	ll2.append(n2)
-	ll2.append(n4)
-	ll2.append(n5)
+	ll2.append(1)
+	ll2.append(2)
+	ll2.append(4)
+	ll2.append(5)
 
 	print(ll2)
 	print()
